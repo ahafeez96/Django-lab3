8a74e6358fcdc823ce2d722be89add1ca6b8d91c
ahafeez96 <fezalm10@gmail.com>
2022-02-02 09:20:10 +0200

    lab2

---
+++ pages/models.py
@@ -0,0 +1,17 @@
+from tkinter import CASCADE
+from django.db import models
+
+class Myuser(models.Model):
+    username= models.CharField(max_length=20)
+    password=models.CharField(max_length=10)
+class Intake(models.Model):#ORM
+    id=models.AutoField(primary_key=True)
+    intakename=models.CharField(max_length=30)
+    startdate=models.DateField()
+    enddate=models.DateField()
+class Track(models.Model):
+    name=models.CharField(max_length=50)
+class Trainee:
+    fullname=models.CharField(max_length=20)
+    bdate=models.DateField()
+    intakeid=models.ForeignKey('Intake',on_delete=models.CASCADE)
---
+++ pages/urls.py
@@ -0,0 +1,14 @@
+from unicodedata import name
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.home,name='home'),
+    path('about',views.about,name='about'),
+    path('contact',views.contact,name='contact'),
+    path('login',views.login,name='login'),
+    path('register',views.register,name='register'),
+    path('landing',views.landing,name='landing'),
+    path('insert',views.insert,name='insert'),
+    path('deleteintake/<id>',views.deleteintake,name='deleteintake'),
+    path('updateintake/<id>',views.updateintake,name='updateintake')
+]
